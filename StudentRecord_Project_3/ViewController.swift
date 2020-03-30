2577d420df6a9204b7d5d5d0216aec3c60fbe518
Umer Khan <umar_s66@outlook.com>
2020-03-30 22:44:20 +0500

    Initial Commit

---
+++ StudentRecord_Project_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentRecord_Project_3
+//
+//  Created by Umer Khan on 30/03/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
