a873bd64c32ee7c09c6cd42601c81af99b71af81
Umer Khan <umar_s66@outlook.com>
2020-03-31 00:53:48 +0500

    UI Setup

---
+++ StudentRecord_Project_3/View/StudentRecordCell.swift
@@ -0,0 +1,21 @@
+//
+//  StudentRecordCell.swift
+//  StudentRecord_Project_3
+//
+//  Created by Umer Khan on 31/03/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import UIKit
+
+class StudentRecordCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
