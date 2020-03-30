639beb43fcbed6cb39679beee219738dec056b6e
Umer Khan <umar_s66@outlook.com>
2020-03-31 01:07:33 +0500

    Work on UI

---
+++ StudentRecord_Project_3/Controller/FormViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FormViewController.swift
+//  StudentRecord_Project_3
+//
+//  Created by Umer Khan on 31/03/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
