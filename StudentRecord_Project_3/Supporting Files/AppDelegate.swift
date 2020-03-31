d4440faea5d389f061bd3be5269cf484dc6ef77a
Umer Khan <umar_s66@outlook.com>
2020-03-31 16:26:22 +0500

    issue fixed

--- StudentRecord_Project_3/Supporting Files/AppDelegate.swift
+++ StudentRecord_Project_3/Supporting Files/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "StudentRecord")
+        let container = NSPersistentContainer(name: "Student")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
